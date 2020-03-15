35a79517668fa1bf79f3dbfe150902fcf489a2d0
wangweihua <wangweihua@vipkid.com.cn>
2020-03-15 21:07:17 +0800

    big bang

---
+++ meta.js
@@ -0,0 +1,39 @@
+const $fs = require('fs');
+const $path = require('path');
+module.exports = {
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "label": "Project name, dash separated words, no space or punctuations"
+    },
+    "description": {
+      "type": "string",
+      "required": true,
+      "label": "Project description",
+      "default": "A Cocos Creator Custom Panel"
+    },
+    "author": {
+      "type": "string",
+      "label": "Author"
+    },
+    "panelTitle": {
+      "type": "string",
+      "label": "Panel title"
+    }
+  },
+  complete: function(data opts) {
+    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName);
+    if (data.name.match(/comp-/)){
+      $fs.unlinkSync($path.resolve(cwd, 'util.js'));
+      $fs.unlinkSync($path.resolve(cwd, 'index.js'));
+    } else if (data.name.match(/util-/)) {
+      $fs.unlinkSync($path.resolve(cwd, 'comp.js'));
+      $fs.unlinkSync($path.resolve(cwd, 'index.js'));
+    } else {
+      $fs.unlinkSync($path.resolve(cwd, 'comp.js'));
+      $fs.unlinkSync($path.resolve(cwd, 'util.js'));
+    }
+    opts.chalk.green('Done!');
+  }
+};
